950c07fc8182a06aada527c54f610d9c66cd334b
Cryptomnesia <82593272+CryptomnesiaWasTaken@users.noreply.github.com>
2023-06-17 20:18:23 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,73 +1,59 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDiscord } from '@fortawesome/free-brands-svg-icons';
 import clipboardCopy from 'clipboard-copy';
 import './App.css'; 
-import optionsJSON from './options.json'; // Import JSON file
+import optionsJSON from './options.json'; 
 
 function App() {
   const [darkMode] = useState(localStorage.getItem("darkMode") !== "false");
   const [prompt, setPrompt] = useState("");
   const [jailbreakName, setJailbreakName] = useState("DEVELOPER MODE");
-  const [options, setOptions] = useState({}); // New state variable for options
   const [buttonText, setButtonText] = useState("Generate");
+  const [topPrompt, setTopPrompt] = useState("");
+  const [options] = useState(optionsJSON);
+  const textAreaRef = useRef(null);
+  const [scrollTrigger, setScrollTrigger] = useState(false);  // new state variable
 
   useEffect(() => {
     localStorage.setItem("darkMode", darkMode);
   }, [darkMode]);
 
-  // Load the options from the JSON file
   useEffect(() => {
-    setOptions(optionsJSON);
+    const selectedOption = options[jailbreakName] || '';
+    setPrompt(selectedOption + topPrompt);
+    setScrollTrigger(true);  // trigger scroll effect
+  }, [topPrompt, jailbreakName]);
+
+  useEffect(() => {
+    if (scrollTrigger) {
+      textAreaRef.current.scrollTop = textAreaRef.current.scrollHeight;
+      setScrollTrigger(false);  // reset trigger
+    }
+  }, [scrollTrigger]);  // scroll effect runs when scrollTrigger changes
+
+  useEffect(() => {
+    textAreaRef.current.scrollTop = textAreaRef.current.scrollHeight;
   }, []);
 
-  const handleGenerate = () => {
-    let generatedText = "";
+  const handleSelectChange = e => {
+    const selectedOption = e.target.value;
+    setJailbreakName(selectedOption);
+    setPrompt(options[selectedOption] || '');
+  };
 
-    switch(jailbreakName) {
-      case "DEVELOPER MODE":
-        generatedText = `${options['DEVELOPER MODE'] || ''}${prompt}`;
-        break;
-      case "BETTER DAN":
-        generatedText = `${options['BETTER DAN'] || ''}${prompt}`;
-        break;
-      case "DEVIL'S ADVOCATE":
-        generatedText = ` ${options["DEVIL'S ADVOCATE"] || ''}${prompt}`;
-        break;
-      case "COMPULSIVE LIAR":
-        generatedText = `${options['COMPULSIVE LIAR'] || ''}${prompt}`;
-        break;
-      case "2-WAY CONVERSATION":
-        generatedText = `${options['2-WAY CONVERSATION'] || ''}${prompt}`;
-        break;
-      case "3-RD PERSON CONVERSATION":
-        generatedText = `${options['3-RD PERSON CONVERSATION'] || ''}${prompt}`;
-        break;
-      case "LITTLEGPT":
-        generatedText = `${options['LITTLEGPT'] || ''}${prompt}`;
-        break;
-      case "AIMBOT":
-        generatedText = `${options['AIMBOT'] || ''}${prompt}`;
-        break;
-      case "POLITICALGPT":
-        generatedText = `${options['POLITICALGPT'] || ''}${prompt}`;
-        break;
-      case "METHBAKE":
-        generatedText = `${options['METHBAKE'] || ''}${prompt}`;
-        break;
-      case "CONSPIRACYGPT":
-        generatedText = `${options['CONSPIRACYGPT'] || ''}${prompt}`;
-        break;
-      default:
-        break;
-    }
+  const handleGenerate = () => {
+    let optionPrefix = options[jailbreakName] || '';
+    let generatedText = optionPrefix + topPrompt;
 
     clipboardCopy(generatedText);
-  
-  setButtonText("Copied!");
-  
-  setTimeout(() => {
-    setButtonText("Generate");
-  }, 2000);  // Change back to "Generate" after 2 seconds
-};
+
+    setButtonText("Copied!");
+
+    setTimeout(() => {
+        setButtonText("Combine + Copy");
+    }, 2000);
+  };
 
   return (
     <div className={darkMode ? "appWrapperDark" : "appWrapper"}>
@@ -76,29 +62,38 @@ function App() {
         <select 
           className={darkMode ? "selectDark" : "select"}
           value={jailbreakName} 
-          onChange={e => setJailbreakName(e.target.value)}
+          onChange={handleSelectChange}
         >
-          <option value="DEVELOPER MODE">Developer Mode</option>
-          <option value="BETTER DAN">Better DAN</option>
-          <option value="DEVIL'S ADVOCATE">Devil's Advocate</option>
-          <option value="COMPULSIVE LIAR">Compulsive Liar</option>
-          <option value="2-WAY CONVERSATION">2-Way Conversation</option>
-          <option value="3-RD PERSON CONVERSATION">3rd Person Conversation</option>
-          <option value="LITTLEGPT">LittleGPT</option>
-          <option value="AIMBOT">AimBot</option>
-          <option value="POLITICALGPT">PoliticalGPT</option>
-          <option value="METHBAKE">Methbake</option>
-          <option value="CONSPIRACYGPT">ConspiracyGPT</option>
+            <option value="DEVELOPER MODE">Developer Mode</option>
+            <option value="BETTER DAN">Better DAN</option>
+            <option value="DEVIL'S ADVOCATE">Devil's Advocate</option>
+            <option value="COMPULSIVE LIAR">Compulsive Liar</option>
+            <option value="2-WAY CONVERSATION">2-Way Conversation</option>
+            <option value="3-RD PERSON CONVERSATION">3rd Person Conversation</option>
+            <option value="LITTLEGPT">LittleGPT</option>
+            <option value="AIMBOT">AimBot</option>
+            <option value="POLITICALGPT">PoliticalGPT</option>
+            <option value="METHBAKE">Methbake</option>
+            <option value="CONSPIRACYGPT">ConspiracyGPT</option>
         </select>
       </div>
       <textarea 
+        className={darkMode ? "textareaDark" : "textarea"}
+        value={topPrompt} 
+        onChange={e => setTopPrompt(e.target.value)}
+      />
+      <textarea 
+        ref={textAreaRef}
         className={darkMode ? "textareaDark" : "textarea"}
         value={prompt} 
         onChange={e => setPrompt(e.target.value)} 
         maxLength={4096}
       />
-      <p className="characterCount">{prompt.length} / 4096 characters</p>
+      <p className="characterCount">{(topPrompt.length + prompt.length)} / 4096 characters</p>
       <button onClick={handleGenerate} className={darkMode ? "genButtonDark" : "genButton"}>{buttonText}</button>
+      <button className={darkMode ? "discordButtonDark" : "discordButton"} onClick={() => window.open('https://discord.gg/your-discord-invite-link', '_blank')}>
+        <FontAwesomeIcon icon={faDiscord} size="8x" color={'#ffffff'} /> {/* Adjust size here */}
+      </button>
     </div>
   );
 }
